111db0cd09087bc79282d101abc1b5f4f69090c8
obinna E <obinnaekenonu@gmail.com>
2023-11-12 20:48:57 +0000

    added documentation to file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
+"""Module to control data storage"""
+
 from models.base_model import BaseModel
 import json
 from models.user import User
 
+
 class FileStorage:
-	__file_path = 'file.json'
-	__objects = {}
+    """Controls how objects are stored"""
 
-	def all(self):
-		return self.__objects
+    __file_path = 'file.json'
+    __objects = {}
 
-	def new(self, obj):
-		key = f'{obj.__class__.__name__}.{obj.id}'
-		self.__objects[key] = obj
+    def all(self):
+        """Returns all instances"""
+        return self.__objects
 
-	def  save(self):
-		with open(self.__file_path, 'w', encoding='UTF-8') as file:
-			data = {k : v.to_dict() for k, v in self.__objects.items()}
-			json.dump(data, file)
+    def new(self, obj):
+        """Creates a new instance object"""
+        key = f'{obj.__class__.__name__}.{obj.id}'
+        self.__objects[key] = obj
 
-	def reload(self):
-		try:
-			with open(self.__file_path, 'r', encoding='utf-8') as file:
-				dict_obj = json.load(file)
-				for val in dict_obj.values():
-					class_name = val["__class__"]
-					del val["__class__"]
-					self.new(eval(f"{class_name}")(**val))
-		except FileNotFoundError:
-			return
+    def save(self):
+        """Save object from memory in json file"""
+        with open(self.__file_path, 'w', encoding='UTF-8') as file:
+            data = {k: v.to_dict() for k, v in self.__objects.items()}
+            json.dump(data, file)
+
+    def reload(self):
+        """Retrieves from json file stored objects"""
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                dict_obj = json.load(file)
+                for val in dict_obj.values():
+                    class_name = val["__class__"]
+                    del val["__class__"]
+                    self.new(eval(f"{class_name}")(**val))
+        except FileNotFoundError:
+            return
